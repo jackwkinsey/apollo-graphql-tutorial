6f1b5a4b9fa0d361c22a43b3e238b68ac247b62d
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-06-04 22:41:40 -0400

    feat: hook up datasources to Apollo server

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,18 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema.js');
+const { createStore } = require('./utils.js');
+const LaunchAPI = require('./datasources/launch.js');
+const UserAPI = require('./datasources/user.js');
 
-const server = new ApolloServer({ typeDefs });
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}!`);
